c441eb5b070fa03fca05c734901c13fb07cdd883
ADMIN <ADMIN@DESKTOP-C78HORB>
2020-09-18 23:05:23 +0530

    connection

--- FSITJDBC/src/main/java/com/manipal/FSITJDBC/service/CoustomerServiceImpl.java
+++ FSITJDBC/src/main/java/com/manipal/FSITJDBC/service/CoustomerServiceImpl.java
@@ -1,13 +1,27 @@
 package com.manipal.FSITJDBC.service;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 
 import com.manipal.FSITJDBC.model.Coustomer;
 
 public class CoustomerServiceImpl implements CustmorService {
+	String driver = "com.mysql.jdbc.Driver";
+	String url = "jdbc:mysql://localhost";
+	String uname ="root";
+	String pass = "root";
 
 	public List<Coustomer> getCoustomer() {
 		// TODO Auto-generated method stub
+		try
+		{
+			Class.forName(driver);
+			Connection conn = DriverManager.getConnection(url,uname,pass);
+			}catch(ClassNotFoundException e) {
+			
+		}
 		return null;
 	}
 
